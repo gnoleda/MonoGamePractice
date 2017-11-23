efe85baf386287b8b4ccfc724f79213279480956
Aubrey Delong <aubrey.delong@gmail.com>
2017-11-22 22:47:12 -0500

    new practice file

---
+++ MonoStringPractice/MonoStringPractice/SimpleString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoStringPractice
+{
+    public class SimpleString : DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+        private SpriteFont font;
+        private string message;
+        private Vector2 position;
+        private Color color;
+
+        public SimpleString(Game game, SpriteBatch spriteBatch, SpriteFont font, string message, Vector2 position, Color color) : base(game)
+        {
+            this.spriteBatch = spriteBatch;
+            this.font = font;
+            this.message = message;
+            this.position = position;
+            this.color = color;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, message, position, color);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
