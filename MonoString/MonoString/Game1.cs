1957d970724a421ae13996c4acbada87d5a2c7cc
Aubrey Delong <aubrey.delong@gmail.com>
2017-11-22 21:51:01 -0500

    first document made learning mono game

---
+++ MonoString/MonoString/Game1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoString
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        //always load stuff in game1, and supply it to objects
+        SpriteFont font;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            //this is where we load the font
+            font = this.Content.Load<SpriteFont>("SimpleFont");
+            MySimpleString s1 = new MySimpleString(this,spriteBatch,font,"hello simple string", new Vector2(100,50), Color.Bisque);
+            MySimpleString s2 = new MySimpleString(this, spriteBatch, font, "hello simple string again", new Vector2(100, 150), Color.AliceBlue);
+            MySimpleString s3 = new MySimpleString(this, spriteBatch, font, "hello simple string again", new Vector2(100, 250), Color.Violet);
+
+            //VERY VERY IMPORTANT
+            //after creating object, we are adding it to a components list.
+            //need to add it to a list in order to make it visible to us
+            this.Components.Add(s1);
+            this.Components.Add(s2);
+            this.Components.Add(s3);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoString/MonoString/MySimpleString.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+//the former 3 lines copy/pasted from game1
+
+namespace MonoString
+{
+    //make it public
+    //my simple string must inherit from drawablegamecomponent
+    public class MySimpleString : DrawableGameComponent
+    {
+        //declare global vars
+        private SpriteBatch spriteBatch;
+        private SpriteFont font;
+        private string message;
+        private Vector2 position;
+        private Color color;
+
+        //call base class constructor :base(game)
+        public MySimpleString(Game game, SpriteBatch spriteBatch, SpriteFont font, string message, Vector2 position, Color color) : base(game)
+        {
+            //save incoming parameters to local variables below
+            this.spriteBatch = spriteBatch;
+            this.font = font;
+            this.message = message;
+            this.position = position;
+            this.color = color;
+
+        }
+
+        //if you have drawable game component, you always have to override update and draw
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        //you must do this with drawable game components
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, message, position, color);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
